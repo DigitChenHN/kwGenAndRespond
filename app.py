b2e313a1106cebe68acb2599a05e61b0a4c329f8
DigitChenHN <chenhn2100@foxmail.com>
2025-03-03 20:21:12 +0800

    25-3-3-20 on desktop

--- app.py
+++ app.py
@@ -4,6 +4,28 @@ import requests
 from bs4 import BeautifulSoup
 import random
 
+import configparser 
+import time
+import re 
+import os 
+import json 
+import random 
+
+from sparkai.llm.llm import ChatSparkLLM, ChunkPrintHandler
+from sparkai.core.messages import ChatMessage
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+SPARKAI_URL = config.get('api', 'url')
+#星火认知大模型调用秘钥信息
+SPARKAI_APP_ID = config.get('api', 'app_id')
+SPARKAI_API_SECRET = config.get('api', 'api_secret')
+SPARKAI_API_KEY = config.get('api', 'api_key')
+#星火认知大模型domain值
+SPARKAI_DOMAIN = config.get('api', 'domain')
+NEWS_URL = config.get('news', 'url')
+
 URL = 'https://news.sina.com.cn/'
 
 def get_title(url):
@@ -29,12 +51,57 @@ def get_title(url):
     
     return texts
 
+class KeywordGen():
+    def __init__(self, news_url:str, news_path:str, max_try:int):
+        self.today_news = news_path
+        self.news_url = news_url 
+
+        if os.path.exists(path=self.today_news):
+            with open(self.today_news, 'r') as file:
+                self.titles = json.load(file)
+            if len(self.titles) == 0:
+                # 如果本地的新闻标题为空，则通过网络获取标题
+                self.titles = get_news_title(self.news_url, max_try) 
+        else:
+            # 如果本地没有新闻标题，则通过网络获取标题
+            self.titles = get_news_title(self.news_url, max_try)
+    
+    def get_keyword_list(self, sparkLLM, keyword_number:int):
+        title = random.choice(self.titles) 
+        self.titles.remove(title) 
+
+        messages = [ChatMessage(
+        role="user",
+        content=f'这是今天新闻的标题{title}，为了能够更好的了解这则新闻的内容，\
+需要你从这个新闻标题延申，生成{keyword_number}个相关的关键词或句子，方便我在搜索引擎搜索，\
+注意，不要简单的摘取关键词，要进行延申提问。\
+每一个生成的关键词或句子用*标记出来，格式为*关键词或句子*，仅回复关键词或句子即可，不要回复其他内容，\
+生成的关键词的数量不要超过{keyword_number}'
+    )]
+        handler = ChunkPrintHandler()
+        a = sparkLLM.generate([messages], callbacks=[handler])
+
+        reply = a.generations[0][0].text
+        klist = re.findall(r'\*(.+?)\*', reply)
+        klist = [s for s in klist if s.strip()]
+        klist = klist[:min(keyword_number,len(klist))]
+        return klist 
+
 app = Flask(__name__)
 
 @app.route('/news_title', methods=['GET'])
 def get_news_title():
+
+    max_trys = 3
     
     n = request.args.get('n', type=int, default=None) 
+
+    date = time.localtime()
+    when = time.strftime('%Y-%m-%d', date) 
+    today_file = when + '.json'
+    path = os.path.join('./daily_news', today_file) 
+
+    generator = KeywordGen(URL, path, max_trys) 
 
     titles = get_title(URL)
     if not isinstance(titles, list):

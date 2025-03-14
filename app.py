124719b7b9f650d2db6c6d4fa9ee952cfeccfa49
ChenHNALY <ChenHN2100@foxmail.com>
2025-03-14 16:40:51 +0800

    version 2, use spark llm to generate keywords and return to client

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ from sparkai.llm.llm import ChatSparkLLM, ChunkPrintHandler
 from sparkai.core.messages import ChatMessage
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+script_dir = os.path.dirname(os.path.abspath(__file__))
+config_dir = os.path.join(script_dir, 'config.ini')
+config.read(config_dir)
 
 SPARKAI_URL = config.get('api', 'url')
 #星火认知大模型调用秘钥信息
@@ -28,26 +30,33 @@ NEWS_URL = config.get('news', 'url')
 
 URL = 'https://news.sina.com.cn/'
 
-def get_title(url):
-    response = requests.get(url)
-    response.encoding = 'utf-8'
+def get_news_title(url, trys:int):
+    times = 0
+    while times < trys:
 
-    # 检查请求是否成功（状态码200表示成功）
-    if response.status_code == 200:
-        # 获取响应的HTML内容
-        html_content = response.text
+        response = requests.get(url)
+        response.encoding = 'utf-8'
+    
+        # 检查请求是否成功（状态码200表示成功）
+        if response.status_code == 200:
+            # 获取响应的HTML内容
+            html_content = response.text 
 
-        # 使用BeautifulSoup解析HTML内容
-        soup = BeautifulSoup(html_content, 'html.parser')
+            # 使用BeautifulSoup解析HTML内容
+            soup = BeautifulSoup(html_content, 'html.parser')
 
-        # 查找所有<a>标签
-        a_tags = soup.find_all('a', target='_blank')
+            # 查找所有<a>标签
+            a_tags = soup.find_all('a', target='_blank')
 
-        # 提取这些标签中的文本并保存到一个列表中
-        texts = [tag.get_text() for tag in a_tags if len(tag.get_text()) > 12]
+            # 提取这些标签中的文本并保存到一个列表中
+            texts = [tag.get_text() for tag in a_tags if len(tag.get_text()) > 12]
+
+            break 
         
-    else:
-        texts = response.status_code
+        else:
+            texts = response.status_code
+            print(f'Request failed, status code {texts}!')
+            trys += 1 
     
     return texts
 
@@ -90,7 +99,7 @@ class KeywordGen():
 app = Flask(__name__)
 
 @app.route('/news_title', methods=['GET'])
-def get_news_title():
+def main():
 
     max_trys = 3
     
@@ -99,21 +108,32 @@ def get_news_title():
     date = time.localtime()
     when = time.strftime('%Y-%m-%d', date) 
     today_file = when + '.json'
-    path = os.path.join('./daily_news', today_file) 
+    path = os.path.join(script_dir, 'daily_news', today_file) 
 
     generator = KeywordGen(URL, path, max_trys) 
 
-    titles = get_title(URL)
-    if not isinstance(titles, list):
+    if not isinstance(generator.titles, list):
         return jsonify({"error": f"Can't get touch with {URL}"})
-    # 检查n是否有效
-    if n is None or n < 0 or n > len(titles):
-        return jsonify({"error": "Invalid value for 'n'. It must be a non-negative integer less than or equal to the length of the list."}), 400
     
-    # 从预定义列表中随机选择n个元素（不重复）
-    result = random.sample(titles, n)
+    spark = ChatSparkLLM(
+        spark_api_url=SPARKAI_URL,
+        spark_app_id=SPARKAI_APP_ID,
+        spark_api_key=SPARKAI_API_KEY,
+        spark_api_secret=SPARKAI_API_SECRET,
+        spark_llm_domain=SPARKAI_DOMAIN,
+        streaming=False,
+    )
+    try: 
+        result = generator.get_keyword_list(spark, n)
+    except Exception as e:
+        return jsonify({'error': "llm goes wrong, try later."})
 
+    with open(path, 'w') as file:
+                json.dump(generator.titles, file)
+                
     return jsonify({'result': result})
+
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  
